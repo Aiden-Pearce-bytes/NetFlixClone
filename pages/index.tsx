f440a2fb29c190ac2e1b4345653eafc4475c9090
AidenPearce <dineshkumarr0597@gmail.com>
2023-04-04 14:48:37 +0530

    Final

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
-import useCurrentUser from "@/hooks/useCurrentUser";
-import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
+import { NextPageContext } from "next";
+
+import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useFavorites from "@/hooks/useFavorites";
+import useMovieList from "@/hooks/useMovieList";
+import InfoModal from "@/components/InfoModal";
+import useInfoModal from "@/hooks/useInfoModal";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -20,15 +28,18 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
-
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+  const {isOpen, closeModal} = useInfoModal();
   return (
     <>
-      <h1 className="text-4xl text-green-500">NetFlix Clone</h1>
-      <p className="text-white">Logged in as:{user?.name}</p>
-      <button className="h-10 w-full bg-white mt-10" onClick={() => signOut()}>
-        Logout!
-      </button>
+      <InfoModal visible = {isOpen} onClose={closeModal} />
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
+      </div>
     </>
   );
 }
